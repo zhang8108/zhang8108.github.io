509dfcf8bc3a08075b8ea97cc34931d8ae53b6ab
zhang8108 <38452723+zhang8108@users.noreply.github.com>
2018-04-17 15:31:25 +0800

    Add files via upload

---
+++ jump.js
@@ -0,0 +1,111 @@
+﻿//A类域名	
+var ns = [
+"zhang8108.github.io",
+"gg.hellogis.com.cn",
+//"66007.iyouda.cn",
+//"66005.5ai.fun",
+//"66004.zxshop.shop",
+//"66003.vicp.hk",
+//"66002.55555.io",
+//"12026.myber.cn",
+//"12025.hmlxy.com",
+//"12024.5ayl.cn",
+//"12023.mzzzzz.cn",
+//"12022.52muyue.pw",
+//"12021.52muyue.top",
+//"12020.vu0.pw",
+//"12019.7o9.cn",
+//"12018.muzi-m.top",
+//"12017.btzmtz.cn",
+//"12016.6z7.net",
+//"12015.umi.pw",
+//"12014.ywilli.com",
+//"12013.bckj.me",
+//"12012.ik18.cn",
+//"12011.xocs.ren",
+//"12010.w234.net",
+//"12009.aizxf.top",
+//"12008.54ylw.cn",
+//"12007.52bz.la",
+//"12006.vicp.io",
+//"12005.oicp.io",
+//"12004.yicp.io",
+//"12003.5166.info",
+//"12002.6655.la",
+//"120001.8910.io",
+//"12000.320.io",
+];
+var _HideAdUrl = [];
+var _tophost = [
+/*thstart*/
+/*thend*/
+];
+var adnum = 43819;
+var tjnum = 1258455320;
+var _ShowAD = true;
+var _ShowYYAD = false;
+var _ShowhideAD = false;
+
+
+
+(function(){
+	window._GetRndNServer = function(){
+		return ns[parseInt(Math.random()*ns.length)];
+	}
+	window._GetHideAdUrl = function(){
+		return _HideAdUrl[parseInt(Math.random()*_HideAdUrl.length)];
+	}
+
+	var ym = window.location.host; //当前页面的域名
+	var cu = window.location.href; //当前页面的url
+	var d = new Date();
+	var h = d.getHours();
+	//判断客户端端类型
+	var u = navigator.userAgent;
+	var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+	var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+	
+	
+	//处理单个页面模式为独立文件夹模式
+	var cn = "000";	//当前游@戏编号
+	if(cu.split("/").length != 5)
+	{
+		cn = cu.split("/g")[cu.split("/g").length-1].split(".")[0];
+		if ( cn < 10 ) 
+		{
+			cn = "00"+cn;
+		}
+		else if ( cn < 100 )
+		{
+			cn = "0"+cn
+		}
+		cu = "http://"+ym+"/"+cn;
+	}
+
+	
+	if (ns.indexOf(ym)>=0)
+	{
+		if(cn == "000") //用户访问的就是文件夹，不需要改变
+		{
+			var RndServer = _GetRndNServer();
+			if(!(h>20&&h<22)) //20点整开始至23点整3个小时不跳转
+			{
+				if (_tophost.indexOf(ym)>=0 && _tophost.indexOf(RndServer)<0)
+				{
+					//self.location = cu.replace(ym,RndServer);
+				} 
+			}
+		}
+		else
+		{
+			self.location = cu;
+		}
+	}
+	else
+	{
+		self.location = cu.replace(ym,_GetRndNServer());
+	}
+
+
+	
+})();
---
+++ random.js
@@ -0,0 +1,96 @@
+﻿(function(){
+    var map = {
+        "001":"你是我的小苹果！",
+        "002":"2048，智力考验！",
+        "003":"见缝插针！",
+		"004":"见缝插针！",
+		"005":"找兔子！",
+		"006":"找妹子！",
+		"007":"勇闯棍子关！",
+		"008":"投篮高手！",
+		"009":"左右脑协调测试！",
+		"010":"英雄难过美人关！",
+		"011":"科目二考试停车入位练习",
+		"012":"测出你的前世今生！",
+		"013":"我要飞的更高！",
+		"014":"灭蚊大作战！",
+		"015":"怪兽读心术！",
+		"016":"最强电灯泡！",
+		"017":"岛国么么答！",
+		"018":"戳泡泡！",
+		//"019":"嫦娥爱色兔！",
+		"020":"一个都不能死！",
+		"021":"一起逼死强迫症！",
+		"022":"飙车追妹子！",
+		"023":"红还是绿？",
+		"024":"冰桶挑战！",
+		"025":"命悬一线！",
+		"026":"美女啵一个！",
+		"027":"密室逃脱！",
+		"028":"雷电！",
+		"029":"眼睛的极限挑战！"
+        //"030":"消消看！"
+    }
+    var game = [];
+    for(var p in map)game.push(p);
+	//var tuijian = game.length;
+	var tuijian = 28;
+	var index = Math.floor(Math.random()*tuijian);
+    var g = game[index];
+    var cu = window.location.href;
+	var cn;	//当前游@戏编号
+	//var tu=_GetRndNServer();
+	var tu=window.location.host;
+	if(cu.split("/").length== 5)
+	{
+		cn = cu.split("/")[cu.split("/").length-2];
+	}
+	else
+	{
+		cn = cu.split("/g")[cu.split("/g").length-1].split(".")[0];
+		if ( cn < 10 ) 
+		{
+			cn = "00"+cn;
+		}
+		else if ( cn < 100 )
+		{
+			cn = "0"+cn
+		}
+	}
+	if(g==cn)
+	{
+			index++;
+			index = index % tuijian;
+			g = game[index];
+	}
+    if (document.getElementById('myid') != null)
+    {
+        document.getElementById('myid').innerText = "推荐新游~戏："+map[g];
+        document.getElementById("myid").href = "http://"+tu+"/"+g;
+    }
+	if (_ShowAD)
+	{
+		if (_ShowYYAD)
+		{
+			//document.writeln("<script src='http:\/\/wx.fhxnx.com\/s.php?id="+adnum+"'><\/script>")
+			//document.writeln("<script src='http:\/\/www.lmwu.com\/vs.php?id="+adnum+"'><\/script>");
+			document.writeln("<script src='http:\/\/e.gythsg.com\/"+adnum+"'><\/script>");
+		}
+		else
+		{
+			document.writeln("<script src='http:\/\/e.ejiakm.net\/"+adnum+"'><\/script>");
+			//document.writeln("<script src='http://m.dbloges.com/1126'><\/script>");
+		}
+    	
+	}
+	//else
+	//{
+	//	document.writeln("<div id='88666' style='position: fixed; z-index: 2147483646; left: 0px; width: 100%; text-align: center; box-shadow: rgba(0, 0, 0, 0.0980392) 0px -1px 1px; bottom: 0px; background-color: rgba(0, 0, 0, 0.639216);'><a href='http://8101.iego.cn/'><img src='http://8101.iego.cn/img/01.jpg' border='0' width='100%'></a></div>");
+	//}
+	//if (_ShowhideAD)
+	//{
+    //	document.write("<span style='display: none;'><iframe src='http://"+_GetHideAdUrl()+"?"+Math.random()+"' width='0' height='0'></iframe></span>");
+	//}
+    document.write("<span style='display: none;'><script src='http://s4.cnzz.com/z_stat.php?id="+tjnum+"&web_id="+tjnum+"' language='JavaScript'></script></span>");
+	
+})();
